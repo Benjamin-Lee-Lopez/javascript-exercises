d75b3f8f022777777164509aaf383da6f2d0d6b0
Benjamin-Lee-Lopez <153187670+BaldrOfOdin@users.noreply.github.com>
2024-07-24 15:26:03 -0500

    Alter and solve exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let rString = "";
+    let reps = string.length
+    for (let i = 0; i < reps; i++) {
+        rString += string.slice(-1);
+        string = string.slice(0, -1);
+    }
 
-};
-
+    return rString;
+}
 // Do not edit below this line
 module.exports = reverseString;
