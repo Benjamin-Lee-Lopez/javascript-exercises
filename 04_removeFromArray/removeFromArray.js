8afd08d250dc85c7614fe5806505adc6e28b6ca5
Benjamin-Lee-Lopez <153187670+BaldrOfOdin@users.noreply.github.com>
2024-07-29 18:46:33 -0500

    Alter and solve exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(numList, ...numToRemove) {
+    let newList = [];
+    for (let i = 0; i < numList.length; i++) {
+        if (!numToRemove.includes(numList[i])) {
+            newList.push(numList[i]);
+        }
+    }
+
+    return newList;
 };
 
 // Do not edit below this line
