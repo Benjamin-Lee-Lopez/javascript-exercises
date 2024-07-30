4e6f087e2d57ebbdf37b2eddcc88c64433271f9e
Benjamin-Lee-Lopez <153187670+BaldrOfOdin@users.noreply.github.com>
2024-07-29 21:39:00 -0500

    Alter and solve exercise 05_sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    if (!Number.isInteger(numOne)|| !Number.isInteger(numTwo)) {return "ERROR"};
+    if (numOne < 0 || numTwo < 0) {return "ERROR"};
 
+    let numToAdd = 0;
+    let numFinal = 0;
+    if (numOne < numTwo) {
+        numToAdd = numOne;
+        numFinal = numTwo;
+    } else {
+        numToAdd = numTwo;
+        numFinal = numOne;
+    }
+    let sum = 0;
+    for (let i = numToAdd; numToAdd <= numFinal; numToAdd++) {
+        sum += numToAdd;
+    }
+    return sum;
 };
 
 // Do not edit below this line
