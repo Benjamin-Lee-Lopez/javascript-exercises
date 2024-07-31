2f5fc1445a5a4c562ce2904c12d86531a8e9d12c
Benjamin-Lee-Lopez <153187670+BaldrOfOdin@users.noreply.github.com>
2024-07-31 14:10:30 -0500

    Alter and solve exercise 06_leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(currentYear) {
+    if (currentYear % 400 == 0) {
+        return true;
+    } else if (currentYear % 4 == 0 && currentYear % 100 != 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
