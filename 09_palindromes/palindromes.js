3da0019c845b48cdd96ddb5ef96879b677da4565
Benjamin-Lee-Lopez <153187670+BaldrOfOdin@users.noreply.github.com>
2024-07-31 15:09:39 -0500

    Alter and solve exercise 09_palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[^\w\']/g,"");
+    let reverseStr = str.split("").reverse().join("")
+    if (reverseStr == str) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
