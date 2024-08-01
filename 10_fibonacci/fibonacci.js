1834f80eacc4c63df7046fda2eb5080f59027103
Benjamin-Lee-Lopez <153187670+BaldrOfOdin@users.noreply.github.com>
2024-08-01 13:13:03 -0500

    Alter and solve exercise 10_fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(iterants) {
+    let lastNum = 0;
+    let lastLastNum = 0;
+    let sum = 1;
 
+    if (iterants == 0) {
+        return 0;
+    } else if (iterants < 0) {
+        return "OOPS";
+    }
+    for (let i = 1; i < Number(iterants); i++) {
+        lastLastNum = lastNum;
+        lastNum = sum;
+        sum = lastNum + lastLastNum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
